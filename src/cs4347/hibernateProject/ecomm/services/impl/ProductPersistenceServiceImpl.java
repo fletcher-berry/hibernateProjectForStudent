54400c914a209831f5cac309b4a5657d6584075d
fletcher-berry <32848772+fletcher-berry@users.noreply.github.com>
2017-11-10 19:56:50 -0600

    Implemented basic CRUD operations

--- src/cs4347/hibernateProject/ecomm/services/impl/ProductPersistenceServiceImpl.java
+++ src/cs4347/hibernateProject/ecomm/services/impl/ProductPersistenceServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import cs4347.hibernateProject.ecomm.entity.Customer;
 import cs4347.hibernateProject.ecomm.entity.Product;
 import cs4347.hibernateProject.ecomm.services.ProductPersistenceService;
 import cs4347.hibernateProject.ecomm.util.DAOException;
@@ -22,22 +23,63 @@ public class ProductPersistenceServiceImpl implements ProductPersistenceService
 	@Override
 	public void create(Product product) throws SQLException, DAOException
 	{
+		try {
+			em.getTransaction().begin();
+			em.persist(product);
+			em.getTransaction().commit();
+		}
+		catch (Exception ex) {
+			em.getTransaction().rollback();
+			throw ex;
+		}
 	}
 
 	@Override
 	public Product retrieve(Long id) throws SQLException, DAOException
 	{
-		return null;
+		try {
+			em.getTransaction().begin();
+			Product employee = em.find(Product.class, id);
+			em.getTransaction().commit();
+			return employee;
+		}
+		catch (Exception ex) {
+			em.getTransaction().rollback();
+			throw ex;
+		}
 	}
 
 	@Override
 	public void update(Product product) throws SQLException, DAOException
 	{
+		try {
+			em.getTransaction().begin();
+			Product prod = em.find(Product.class, product.getId());
+			prod.setProdCategory(product.getProdCategory());
+			prod.setProdDescription(product.getProdDescription());
+			prod.setProdName(product.getProdName());
+			prod.setProdUPC(product.getProdUPC());
+			em.getTransaction().commit();
+		}
+		catch (Exception ex) {
+			em.getTransaction().rollback();
+			throw ex;
+		}
 	}
 
 	@Override
 	public void delete(Long id) throws SQLException, DAOException
 	{
+		try {
+			em.getTransaction().begin();
+			Product prod = (Product)em.find(Product.class, id);
+			em.remove(prod);
+			em.getTransaction().commit();
+		}
+		catch (Exception ex) {
+			em.getTransaction().rollback();
+			throw ex;
+		}
 	}
 
 	@Override
