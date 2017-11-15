6201f4356a35c1a8b12d0f07fa12a1b59e904ae8
fletcher-berry <32848772+fletcher-berry@users.noreply.github.com>
2017-11-14 20:14:29 -0600

    Added comments to PurchasePersistanceServiceImpl

--- src/cs4347/hibernateProject/ecomm/services/impl/ProductPersistenceServiceImpl.java
+++ src/cs4347/hibernateProject/ecomm/services/impl/ProductPersistenceServiceImpl.java
@@ -11,6 +11,7 @@ import cs4347.hibernateProject.ecomm.entity.Product;
 import cs4347.hibernateProject.ecomm.services.ProductPersistenceService;
 import cs4347.hibernateProject.ecomm.util.DAOException;
 
+// coded by Fletcher
 public class ProductPersistenceServiceImpl implements ProductPersistenceService
 {
 	@PersistenceContext 
@@ -24,11 +25,13 @@ public class ProductPersistenceServiceImpl implements ProductPersistenceService
 	public void create(Product product) throws SQLException, DAOException
 	{
 		try {
-			em.getTransaction().begin();
+			// persist the product and commit transaction
+			em.getTransaction().begin();	
 			em.persist(product);
 			em.getTransaction().commit();
 		}
 		catch (Exception ex) {
+			// if there is a failure, rollback
 			em.getTransaction().rollback();
 			throw ex;
 		}
@@ -38,12 +41,14 @@ public class ProductPersistenceServiceImpl implements ProductPersistenceService
 	public Product retrieve(Long id) throws SQLException, DAOException
 	{
 		try {
+			// retrieve product by id
 			em.getTransaction().begin();
 			Product employee = em.find(Product.class, id);
 			em.getTransaction().commit();
 			return employee;
 		}
 		catch (Exception ex) {
+			// if there is a failure, rollback
 			em.getTransaction().rollback();
 			throw ex;
 		}
@@ -53,15 +58,16 @@ public class ProductPersistenceServiceImpl implements ProductPersistenceService
 	public void update(Product product) throws SQLException, DAOException
 	{
 		try {
-			em.getTransaction().begin();
-			Product prod = em.find(Product.class, product.getId());
-			prod.setProdCategory(product.getProdCategory());
+			em.getTransaction().begin();							// start transaction
+			Product prod = em.find(Product.class, product.getId());	// find the product
+			prod.setProdCategory(product.getProdCategory());		// set the product's attributes
 			prod.setProdDescription(product.getProdDescription());
 			prod.setProdName(product.getProdName());
-			prod.setProdUPC(product.getProdUPC());
-			em.getTransaction().commit();
+			prod.setProdUPC(product.getProdUPC());		
+			em.getTransaction().commit();							// commit changes
 		}
 		catch (Exception ex) {
+			// if there is a failure, rollback
 			em.getTransaction().rollback();
 			throw ex;
 		}
@@ -71,12 +77,14 @@ public class ProductPersistenceServiceImpl implements ProductPersistenceService
 	public void delete(Long id) throws SQLException, DAOException
 	{
 		try {
+			// remove product and commit transaction
 			em.getTransaction().begin();
 			Product prod = (Product)em.find(Product.class, id);
 			em.remove(prod);
 			em.getTransaction().commit();
 		}
 		catch (Exception ex) {
+			// if there is a failure, rollback
 			em.getTransaction().rollback();
 			throw ex;
 		}
@@ -88,15 +96,18 @@ public class ProductPersistenceServiceImpl implements ProductPersistenceService
 		try 
 		{
 			em.getTransaction().begin();
+			// retrieve a product by UPC
 			List<Product> products = em.createQuery("from Product as p where p.prodUPC = :upc")
 					.setParameter("upc", upc).getResultList();
-			if(products.size() != 1)
+			em.getTransaction().commit();
+			if(products.size() != 1)	// return null if UPC not found
 				return null;
 			return products.get(0);
 				
 		}
 		catch (Exception ex) 
 		{
+			// if there is a failure, rollback
 			em.getTransaction().rollback();
 			throw ex;
 		}
@@ -107,14 +118,17 @@ public class ProductPersistenceServiceImpl implements ProductPersistenceService
 	{
 		try 
 		{
+			// retrieve products by category
 			em.getTransaction().begin();
 			List<Product> products = em.createQuery("from Product as p where p.prodCategory = :category")
 					.setParameter("category", category).getResultList();
+			em.getTransaction().commit();
 			return products;
 			
 		}
 		catch (Exception ex) 
 		{
+			// if there is a failure, rollback
 			em.getTransaction().rollback();
 			throw ex;
 		}
